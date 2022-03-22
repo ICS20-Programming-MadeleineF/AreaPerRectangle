c0f50636f8b47ab418e20c03de5954db48300c6f
Madeleine Forgeron <99274168+MadeleineForgeron@users.noreply.github.com>
2022-03-22 15:05:50 +0000

    do css

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Madeleine Forgeron All rights reserved
+//
+// Created by: Madeleine Forgeron
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calcAreaPer () {
+  // this function displays area and perimeter"
+  
+  document.getElementById('calc-area').innerHTML = 'The area is ' + (17*34) + 'm²'
+	document.getElementById('calc-per').innerHTML = 'The perimeter is ' + (2*17 + 2*34) + 'm'
+}
